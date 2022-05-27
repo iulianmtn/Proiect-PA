40edfe97ac2f9958945392f24a5b09e893464ee7
Claudiu-Gavril Stan <stanclaudiu321@gmail.com>
2022-05-27 15:47:29 +0300

    "Now I am up to date"

---
+++ URClient/src/main/java/model/Room.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Room {
+    String name;
+    int capacity;
+
+    public Room(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
