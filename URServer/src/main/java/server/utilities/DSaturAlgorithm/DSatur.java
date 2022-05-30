941a4768d54d557354b5e68c6b02f2db30fc6177
Claudiu-Gavril Stan <stanclaudiu321@gmail.com>
2022-05-30 16:46:35 +0300

    Deleted the souts and refactored the algorithm a little bit more beatiful

--- URServer/src/main/java/server/utilities/DSaturAlgorithm/DSatur.java
+++ URServer/src/main/java/server/utilities/DSaturAlgorithm/DSatur.java
@@ -2,69 +2,59 @@ package server.utilities.DSaturAlgorithm;
 
 import server.model.Event;
 import server.model.Room;
-import static server.utilities.entities.helpers.Colours.*;
 
-import java.awt.*;
-import java.util.List;
-import java.util.Queue;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class DSatur {
-    private DSatur(){}
-
     private static final Integer INFINITY = Integer.MAX_VALUE;
 
+    private DSatur() {
+    }
+
     /**
-     * @param inputRooms represents the rooms that we have to insert in database
+     * @param inputRooms  represents the rooms that we have to insert in database
      * @param inputEvents represents the events that we have to make a schedule on
-     * @param idRooms represents a Map used to keep track of the real ids in the database of the inputRooms
+     * @param idRooms     represents a Map used to keep track of the real ids in the database of the inputRooms
      * @return will return a Map that associates for each event its id of Room from database
      */
-    public static Map<Event,Integer> makeSchedule(List<Room> inputRooms, List<Event> inputEvents, Map<Room,Integer> idRooms)
-    {
+    public static Map<Event, Integer> makeSchedule(List<Room> inputRooms, List<Event> inputEvents, Map<Room, Integer> idRooms) {
         /**
          * 1. Let's create the graph as Map<Event,List<Event>>, for each Event we say that we have an adjacent
          *  node if and only if the two Events are between the same hours and the same Day*
          */
 
-        Map<Event,List<Event>> graph = new HashMap<>();
-        Map<Event,Integer> colorGrade = new HashMap<>();
-        Map<Event,Room> distribution = new HashMap<>();
+        Map<Event, List<Event>> graph = new HashMap<>();
+        Map<Event, Integer> colorGrade = new HashMap<>();
+        Map<Event, Room> distribution = new HashMap<>();
 
-        inputEvents.stream().forEach(element->findNeighbors(element,inputEvents,graph));
-        inputEvents.stream().forEach(element->colorGrade.put(element,0));
-        inputEvents.stream().forEach(node->distribution.put(node,null));
+        inputEvents.stream().forEach(element -> findNeighbors(element, inputEvents, graph));
+        inputEvents.stream().forEach(element -> colorGrade.put(element, 0));
+        inputEvents.stream().forEach(node -> distribution.put(node, null));
 
         int numberSteps = inputEvents.size();
 
-        while(numberSteps-- > 0)
-        {
-            Event nextEvent = chooseEvent(graph,colorGrade,distribution);
+        while (numberSteps-- > 0) {
+            Event nextEvent = chooseEvent(graph, colorGrade, distribution);
 
-            distribution.replace(nextEvent,findValidRoom(nextEvent,graph,distribution,inputRooms));
+            distribution.replace(nextEvent, findValidRoom(nextEvent, graph, distribution, inputRooms));
 
-            updateColorGrade(graph,distribution,colorGrade);
+            updateColorGrade(graph, distribution, colorGrade);
 
-            System.out.println("----------Iteration : " +(inputEvents.size()-numberSteps));
         }
 
-        System.out.println(distribution);
 
         List<Event> maybeFictiveSchedule = distribution.keySet().stream().toList();
-        Map<Event,Integer> foreignKeyDistribution = new HashMap<>();
+        Map<Event, Integer> foreignKeyDistribution = new HashMap<>();
 
-        for(Event event : maybeFictiveSchedule)
-        {
-            if(distribution.get(event).getCapacity()==INFINITY)
-            {
-                distribution.replace(event,null);////we did not find a room for this event
-                foreignKeyDistribution.put(event,null);
-            }
-            else    //we try to map It like Event,idRoom
+        for (Event event : maybeFictiveSchedule) {
+            if (distribution.get(event).getCapacity() == INFINITY) {
+                distribution.replace(event, null);////we did not find a room for this event
+                foreignKeyDistribution.put(event, null);
+            } else    //we try to map It like Event,idRoom
             {
                 Room takenRoom = distribution.get(event);
-                foreignKeyDistribution.put(event,idRooms.get(takenRoom));
+                foreignKeyDistribution.put(event, idRooms.get(takenRoom));
             }
         }
 
@@ -73,43 +63,38 @@ public class DSatur {
 
     }
 
-    private static void updateColorGrade(Map<Event,List<Event>> graph,Map<Event,Room> distribution,Map<Event,Integer> colorGrade)
-    {
+    private static void updateColorGrade(Map<Event, List<Event>> graph, Map<Event, Room> distribution, Map<Event, Integer> colorGrade) {
         Integer nbEvents = graph.keySet().size();
         List<Event> events = graph.keySet().stream().toList();
 
-        for(int node = 0; node < nbEvents ;++node)
-        {
+        for (int node = 0; node < nbEvents; ++node) {
             List<Event> neighbors = graph.get(events.get(node));
-            Long numberColoredNeighbors = neighbors.stream().map(event->distribution.get(event)).distinct().filter(room->room!=null).count();
-            if(distribution.get(events.get(node))==null) //for the events that were not scheduled yet
-                colorGrade.replace(events.get(node),numberColoredNeighbors.intValue());
+            Long numberColoredNeighbors = neighbors.stream().map(event -> distribution.get(event)).distinct().filter(room -> room != null).count();
+            if (distribution.get(events.get(node)) == null) //for the events that were not scheduled yet
+                colorGrade.replace(events.get(node), numberColoredNeighbors.intValue());
             else
-                colorGrade.replace(events.get(node),-1);
+                colorGrade.replace(events.get(node), -1);
         }
     }
 
-    private static Room findValidRoom(Event node,Map<Event,List<Event>> graph,Map<Event,Room> distribution,List<Room> colors)
-    {
+    private static Room findValidRoom(Event node, Map<Event, List<Event>> graph, Map<Event, Room> distribution, List<Room> colors) {
         List<Event> neighbors = graph.get(node);
         Integer numberColors = colors.size();
 
-        Room bestFit = new Room("test",INFINITY);
+        Room bestFit = new Room("test", INFINITY);
 
-        for(int nbColor=0;nbColor<numberColors;++nbColor)
-        {
+        for (int nbColor = 0; nbColor < numberColors; ++nbColor) {
             boolean enoughSpace = node.getSize() <= colors.get(nbColor).getCapacity();
-            boolean available = !neighbors.stream().map(nod->distribution.get(nod)).distinct().collect(Collectors.toList()).contains(colors.get(nbColor));
+            boolean available = !neighbors.stream().map(nod -> distribution.get(nod)).distinct().collect(Collectors.toList()).contains(colors.get(nbColor));
             Room candidate = colors.get(nbColor);
-            if(enoughSpace && available && candidate.getCapacity() < bestFit.getCapacity())
+            if (enoughSpace && available && candidate.getCapacity() < bestFit.getCapacity())
                 bestFit = candidate;
         } //assign an existing room
 
-        if(bestFit.getCapacity()!=INFINITY)
+        if (bestFit.getCapacity() != INFINITY)
             return bestFit;
-        if(distribution.get(node)==null)
-        {
-            Room fictiveRoom = new Room("fictive",INFINITY);
+        if (distribution.get(node) == null) {
+            Room fictiveRoom = new Room("fictive", INFINITY);
             colors.add(fictiveRoom);
             return fictiveRoom;
         } //we create a new fictive room to be used so the algorithm can run
@@ -118,59 +103,44 @@ public class DSatur {
     }
 
 
-    private static Event chooseEvent(Map<Event,List<Event>> graph,Map<Event,Integer> colorGrade,Map<Event,Room> distribution)
-    {
-        Integer maxColoration = colorGrade.keySet().stream().map(node->colorGrade.get(node)).max(Integer::compare).orElse(INFINITY);
-        System.out.println("The maxColoration is : "+maxColoration);
-        long countColoredMaxim = colorGrade.keySet().stream().map(node->colorGrade.get(node)).filter(node->(node==maxColoration)).count();
-        System.out.println(ANSI_RED+"The number of maximum colored nodes "+countColoredMaxim+ANSI_RESET);
-        if(countColoredMaxim<1)
+    private static Event chooseEvent(Map<Event, List<Event>> graph, Map<Event, Integer> colorGrade, Map<Event, Room> distribution) {
+        Integer maxColoration = colorGrade.keySet().stream().map(node -> colorGrade.get(node)).max(Integer::compare).orElse(INFINITY);
+        long countColoredMaxim = colorGrade.keySet().stream().map(node -> colorGrade.get(node)).filter(node -> (node == maxColoration)).count();
+        if (countColoredMaxim < 1)
             return null;
-        else
-        if(countColoredMaxim==1)
-        {
-            Event chosenEvent = colorGrade.keySet().stream().max((firstNode,secondNode)->Integer.compare(colorGrade.get(firstNode),colorGrade.get(secondNode))).orElse(null);
-            System.out.println(colorGrade);
-            System.out.println("The chosen event : "+chosenEvent);
+        else if (countColoredMaxim == 1) {
+            Event chosenEvent = colorGrade.keySet().stream().max((firstNode, secondNode) -> Integer.compare(colorGrade.get(firstNode), colorGrade.get(secondNode))).orElse(null);
             return chosenEvent;
-        }
-        else
-        {
-            Map<Event,List<Event>> cleanGraph = graphWithoutColor(graph,distribution);
-            System.out.println(ANSI_BLUE+"-----The clean Graph---"+cleanGraph+ANSI_RESET);
-            List<Event> candidates = colorGrade.keySet().stream().filter(node->colorGrade.get(node)==maxColoration).collect(Collectors.toList());
-            System.out.println(ANSI_GREEN+"Our candidates are : "+candidates+ANSI_RESET);
-            Event bestCandidate = maxInduceSubgraph(candidates,cleanGraph);
-            System.out.println(ANSI_YELLOW+"The best candidate is : "+bestCandidate + " with the following induce "+induceSubgraph(bestCandidate,cleanGraph)+ANSI_RESET);
+        } else {
+            Map<Event, List<Event>> cleanGraph = graphWithoutColor(graph, distribution);
+            List<Event> candidates = colorGrade.keySet().stream().filter(node -> colorGrade.get(node) == maxColoration).collect(Collectors.toList());
+            Event bestCandidate = maxInduceSubgraph(candidates, cleanGraph);
             return bestCandidate;
         }
     }
-    private static Event maxInduceSubgraph(List<Event> candidates,Map<Event,List<Event>> cleanGraph)
-    {
-        Map<Event,Integer> nodeSubgraphValue = new HashMap<>();
-        for(Event candidate : candidates)
-            nodeSubgraphValue.put(candidate,induceSubgraph(candidate,cleanGraph));
 
-        Event bestCandidate = nodeSubgraphValue.keySet().stream().max((firstNode,secondNode)->Integer.compare(nodeSubgraphValue.get(firstNode),nodeSubgraphValue.get(secondNode))).orElse(null);
+    private static Event maxInduceSubgraph(List<Event> candidates, Map<Event, List<Event>> cleanGraph) {
+        Map<Event, Integer> nodeSubgraphValue = new HashMap<>();
+        for (Event candidate : candidates)
+            nodeSubgraphValue.put(candidate, induceSubgraph(candidate, cleanGraph));
+
+        Event bestCandidate = nodeSubgraphValue.keySet().stream().max((firstNode, secondNode) -> Integer.compare(nodeSubgraphValue.get(firstNode), nodeSubgraphValue.get(secondNode))).orElse(null);
 
         return bestCandidate;
     }
-    private static Integer induceSubgraph(Event node,Map<Event,List<Event>> cleanGraph)
-    {
+
+    private static Integer induceSubgraph(Event node, Map<Event, List<Event>> cleanGraph) {
         Queue<Event> orderBfs = new ArrayDeque<>();
         orderBfs.add(node);
         List<Event> visitedNodes = new ArrayList<>();
         visitedNodes.add(node);
 
-        while(!orderBfs.isEmpty())
-        {
+        while (!orderBfs.isEmpty()) {
             Event currentNode = orderBfs.poll();
             List<Event> neighbors = cleanGraph.get(currentNode);
 
-            for(Event neighbor : neighbors)
-            {
-                if(!visitedNodes.contains(neighbor))
-                {
+            for (Event neighbor : neighbors) {
+                if (!visitedNodes.contains(neighbor)) {
                     orderBfs.add(neighbor); ///I will need to explore you as well
                     visitedNodes.add(neighbor); //we visited it
                 }
@@ -180,21 +150,16 @@ public class DSatur {
         return visitedNodes.size(); ///the size of the subgraph
     }
 
-    private  static Map<Event,List<Event>> graphWithoutColor(Map<Event,List<Event>> graph, Map<Event,Room> distribution){
+    private static Map<Event, List<Event>> graphWithoutColor(Map<Event, List<Event>> graph, Map<Event, Room> distribution) {
         List<Event> allNodes = graph.keySet().stream().toList();
-        Map<Event,List<Event>> cleanGraph = new HashMap<>(graph);
-        for(Event node : allNodes)
-        {
-            if(distribution.get(node)!=null)
-            {
-                System.out.println(ANSI_PURPLE+"This event is assigned! " +(node)+ANSI_RESET);
+        Map<Event, List<Event>> cleanGraph = new HashMap<>(graph);
+        for (Event node : allNodes) {
+            if (distribution.get(node) != null)
                 cleanGraph.remove(node);
-            }
-            else
-            {
+            else {
                 List<Event> neighbors = cleanGraph.get(node);
-                neighbors = neighbors.stream().filter(event->distribution.get(event)==null).collect(Collectors.toList());
-                cleanGraph.replace(node,neighbors); ///but I think is redundant
+                neighbors = neighbors.stream().filter(event -> distribution.get(event) == null).collect(Collectors.toList());
+                cleanGraph.replace(node, neighbors); ///but I think is redundant
                 ///we have now a clean part of the graph
             }
         }
@@ -202,17 +167,16 @@ public class DSatur {
     }
 
 
-    private static List<Event> cleanAdjacentList(List<Event> adjacentList,Map<Event,Integer> colorGrade)
-    {
-        return adjacentList.stream().filter(node->colorGrade.get(node)==-1).collect(Collectors.toList());
+    private static List<Event> cleanAdjacentList(List<Event> adjacentList, Map<Event, Integer> colorGrade) {
+        return adjacentList.stream().filter(node -> colorGrade.get(node) == -1).collect(Collectors.toList());
     }
 
-    private static void findNeighbors(Event event, List<Event> eventList,  Map<Event, List<Event>> graph){
-        List<Event> neighbors = eventList.stream().filter(node->adjacentNodes(event,node)).collect(Collectors.toList());
-        graph.put(event,neighbors);
+    private static void findNeighbors(Event event, List<Event> eventList, Map<Event, List<Event>> graph) {
+        List<Event> neighbors = eventList.stream().filter(node -> adjacentNodes(event, node)).collect(Collectors.toList());
+        graph.put(event, neighbors);
     }
 
-    private static boolean adjacentNodes(Event firstEvent,Event secondEvent){
+    private static boolean adjacentNodes(Event firstEvent, Event secondEvent) {
         return firstEvent.getDay().equals(secondEvent.getDay())
                 && (
                 ((firstEvent.getStart() >= secondEvent.getStart() && firstEvent.getStart() < secondEvent.getEnd())
